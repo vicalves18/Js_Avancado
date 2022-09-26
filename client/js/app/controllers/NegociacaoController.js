8ca0a86e5d3b86ae22bf004d7af271a52f45585c
Victoria Alves <vic.alves200216@gmail.com>
2022-09-26 11:34:06 -0300

    Metodo Static e Template Strings

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -14,17 +14,14 @@ class NegociacaoController{
         //evita que a pagina recarregue 
         event.preventDefault();
 
-        let helper = new DateHelper();
-        let data = helper.textoParaData(this._inputData.value);
-
         //Atraves do value que consegue acessar o valor do formulario
         let negociacao = new Negociacao(
-            data,
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
            this._inputValor.value
         );
         console.log(negociacao);
-        console.log(helper.dataParaTexto(negociacao.data));
+        console.log(DateHelper.dataParaTexto(negociacao.data));
 
         //descobrir tipo do parametro passado 
         //console.log(typeof(this._inputData.value));
--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,12 +1,20 @@
 class DateHelper{
-    dataParaTexto(data){
-        return data.getDate() 
-         + '/' + (data.getMonth() + 1)
-         + '/' + data.getFullYear();
+    constructor(){
+        throw new Error('Esta classe não pode ser instanciada');
+    }
+
+    static dataParaTexto(data){
+        //${} Template Strings
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+      
      }
 
-    textoParaData(texto){
-         //... -> Spred Operator, indica que o array é desmembrado
+    static textoParaData(texto){
+        //Fail Fast - falha rápida
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)){
+            throw new Error('Deve estar no formato yyyy-mm-dd');
+        }
+        //... -> Spred Operator, indica que o array é desmembrado
         return new Date(...texto.split('-')
         .map((item,indice)=>item - indice % 2)); //arrow function
     }
